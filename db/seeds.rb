440b37cbc4189ce07eec0001f09c7228d1b44cd3
Steven Cheong <stevenjern@gmail.com>
2016-06-24 15:28:34 -0700

    first commit: initial setup

---
+++ db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Bench.create(description: "AT&T Park Bench", lat: 37.778830, lng: -122.387554)
+Bench.create(description: "Palace of Fine Arts Theatre Bench", lat: 37.801933, lng: -122.447386)
+Bench.create(description: "Marina Green Bench", lat: 37.807457, lng: -122.436472)
+Bench.create(description: "Ghirardelli Square Bench", lat: 37.805845, lng: -122.422404)
+Bench.create(description: "Cupid's Span Bench", lat: 37.791538, lng: -122.390134)
+Bench.create(description: "16th Avenue Tiled Steps Bench", lat: 37.756300, lng: -122.471797)
